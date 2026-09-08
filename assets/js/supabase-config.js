43ac664f3c576f6b287c822113914fcd4cb185b7
sofia212400-a11y <sofia212400@gmail.com>
2026-09-07 22:50:48 -0500

    Correcion de url

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,6 +1,6 @@
 
 
-const SUPABASE_URL = "https://nuoicznqhfmvpxzioksp.supabase.co/rest/v1/";       // ej: https://abcdefgh.supabase.co
+const SUPABASE_URL = "https://nuoicznqhfmvpxzioksp.supabase.co";       // ej: https://abcdefgh.supabase.co
 const SUPABASE_ANON_KEY = "sb_publishable_i4gl5oMAA25amITRp9UGdw_8dIjeKcI";
 
 // Cliente único de Supabase, usado por auth.js y emocional.js.
